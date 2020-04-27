ce80cc3a65848736fb56f2c5c7decab6084beddf
matotec <matias.chaile.1988@gmail.com>
2020-04-27 01:37:09 -0300

    proyecto con pom

---
+++ arena/tp1-unqflix/src/main/kotlin/appUnqflix/ApplicationUnqflix.kt
@@ -0,0 +1,11 @@
+package appUnqflix
+
+import org.uqbar.arena.Application
+import org.uqbar.arena.windows.Window
+
+class ApplicationUnqflix : Application {
+    override fun createMainWindow(): Window<*> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
