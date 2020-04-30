9178b38f9cca95ab8b6815cff179c577a49b2700
fedecame <fede.cameriere@gmail.com>
2020-04-30 05:30:57 -0300

    Chapter casi listo..falta mas q nada definir como manejamos system/UnqflixAppModel desde todos lados. Porque necesitamos usar las funciones del modelo en todos lados.

--- arena/tp1-unqflix/src/main/kotlin/appUnqflix/appModel/ChaptersAppModel.kt
+++ arena/tp1-unqflix/src/main/kotlin/appUnqflix/appModel/ChaptersAppModel.kt
@@ -1,16 +1,24 @@
 package appUnqflix.appModel
 
 import domain.Chapter
+import org.uqbar.commons.model.annotations.Observable
 
+@Observable
 class ChaptersAppModel (var chapter : Chapter) {
     var id: String = ""
-    var description: String = ""
+    var title: String = ""
     var duration: Int = 0
+    var description: String = ""
+    var thumbnail: String = ""
+    var video: String = ""
 
     init {
         this.id = chapter.id
-        this.description = chapter.description
+        this.title = chapter.title
         this.duration = chapter.duration
+        this.description = chapter.description
+        this.thumbnail = chapter.thumbnail
+        this.video = chapter.video
     }
 
 }
