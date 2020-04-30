9a612fc8ae870b7772548e5559d8f8ac05e906c3
fedecame <fede.cameriere@gmail.com>
2020-04-30 02:10:55 -0300

    mini update

--- arena/tp1-unqflix/src/main/kotlin/appUnqflix/appModel/ChaptersAppModel.kt
+++ arena/tp1-unqflix/src/main/kotlin/appUnqflix/appModel/ChaptersAppModel.kt
@@ -2,6 +2,15 @@ package appUnqflix.appModel
 
 import domain.Chapter
 
-class ChaptersAppModel (var chapters : Chapter) {
-    var numeroPrueba = 0
+class ChaptersAppModel (var chapter : Chapter) {
+    var id: String = ""
+    var description: String = ""
+    var duration: Int = 0
+
+    init {
+        this.id = chapter.id
+        this.description = chapter.description
+        this.duration = chapter.duration
+    }
+
 }
