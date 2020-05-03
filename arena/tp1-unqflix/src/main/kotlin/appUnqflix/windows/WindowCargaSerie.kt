62e71dd52e7cf0f193a99d6601a8ef1c9df788c2
Flavia <flaviaivanafernandez@gmail.com>
2020-05-03 02:18:12 -0300

    Se completo la interfaz de la ventana cargar Serie

--- arena/tp1-unqflix/src/main/kotlin/appUnqflix/windows/WindowCargaSerie.kt
+++ arena/tp1-unqflix/src/main/kotlin/appUnqflix/windows/WindowCargaSerie.kt
@@ -9,6 +9,7 @@ import java.awt.Color
 
 class WindowCargaSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWindow<UnqflixAppModel>(owner, model){
 
+
     override fun addActions(p0: Panel?) {
 
     }
@@ -26,10 +27,7 @@ class WindowCargaSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWind
                 TextBox(it) with {
                     fontSize = 10
                     width = 200
-//            alignLeft()  // right, left, center
-//
-//            bgColor = Color.orange
-//            bindTo("falta var")
+                    //   bindToModel(thisWindow.modelObject.selectedSerie!!, "title")
                 }
 
             }
@@ -42,29 +40,23 @@ class WindowCargaSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWind
                 TextBox(it) with {
                     fontSize = 10
                     width = 200
-//            alignLeft()  // right, left, center
-//
-//            bgColor = Color.orange
-//            bindTo("falta var")
+                    //      bindToModel(thisWindow.modelObject.selectedSerie!!, "poster")
                 }
             }
         }
-
-        Panel(p0)with {
+        Label(p0) with {
+            text = "Description:"
+            alignLeft()
+        }
+        Panel(p0) with {
             asHorizontal()
             Panel(it) with {
-                Label(it) with {
-                    text = "Desciption:"
-                    alignLeft()
-                }
 
                 KeyWordTextArea(it) with {
                     height = 100
                     width = 150
-//            bgColor = Color.orange
-//            bindTo("falta var")
-//            bindColorTo("blue")
-//            bindEnabledTo("enabled")
+
+                    //  bindToModel(thisWindow.modelObject.selectedSerie!!, "descripcion")
                 }
             }
 
@@ -74,65 +66,109 @@ class WindowCargaSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWind
                     text = "State"
                     alignLeft()
                 }
-
                 CheckBox(it) with {
-                    //            bindTo("selected")
-//            bindEnabledTo("disabled")
+                    //
+
 
                 }
-
-            }
-        }
-
-
-
-        Panel(p0) with {
-            Label(it) with {
-                text = "Categories:"
-                alignLeft()
             }
 
-            KeyWordTextArea(it) with {
-                height = 100
-                width = 50
+//////////Panel Categorias//////
+
+            Panel(p0) with {
+                Label(it) with {
+                    text = "Categories:"
+                    alignLeft()
+                }
+            }
+            Panel(p0) with {
+                asHorizontal()
+                KeyWordTextArea(it) with {
+                    height = 200
+                    width = 70
+                    bgColor = Color.orange
+//            bindTo("falta var")
+//            bindColorTo("blue")
+//            bindEnabledTo("enabled")
+                }
+                Panel(it) with {
+                    asVertical()
+                    Button(it) with {
+                        text = "<"
+                        fontSize = 10
+                    }
+                    Button(it) with {
+                        text = ">"
+                        fontSize = 10
+                    }
+                }
+                KeyWordTextArea(it) with {
+                    width = 70
+                    height = 200
 //            bgColor = Color.orange
 //            bindTo("falta var")
 //            bindColorTo("blue")
 //            bindEnabledTo("enabled")
-            }
-        }
+                }
 
 
-        Panel(p0) with {
-            Label(it) with {
-                text = "Related content:"
-                alignLeft()
             }
 
-            KeyWordTextArea(it) with {
-                height = 100
-                width = 50
-//            bgColor = Color.orange
+///////////////Related Content
+            Panel(p0) with {
+                asHorizontal()
+                Label(it) with {
+                    text = "Related content:"
+                    alignLeft()
+                }
+            }
+            Panel(p0) with {
+                asHorizontal()
+                KeyWordTextArea(it) with {
+                    height = 100
+                    width = 180
+                    height = 200
+                    bgColor = Color.blue
 //            bindTo("falta var")
 //            bindColorTo("blue")
 //            bindEnabledTo("enabled")
+                }
+                Panel(it) with {
+                    asVertical()
+                    Button(it) with {
+                        text = "<"
+                        fontSize = 10
+                    }
+                    Button(it) with {
+                        text = ">"
+                        fontSize = 10
+                    }
+                }
+                KeyWordTextArea(it) with {
+                    width = 180
+                    height = 200
+                    bgColor = Color.yellow
+//            bindTo("falta var")
+//            bindColorTo("blue")
+//            bindEnabledTo("enabled")
+                }
             }
+
+
+            Panel(p0) with {
+                asHorizontal()
+                Button(it) with {
+                    text = "Accept"
+                    fontSize = 10
+                }
+
+                Button(it) with {
+                    text = "Cancel"
+                    fontSize = 10
+                }
+            }
+
+
         }
 
-        Panel(p0) with {
-            asHorizontal()
-            Button(it) with {
-                text = "Accept"
-                fontSize = 10
-            }
-
-            Button(it) with {
-                text = "Cancel"
-                fontSize = 10
-            }
-        }
-
-
-    }
-
-}
+    }}
--- arena/tp1-unqflix/src/main/kotlin/appUnqflix/windows/WindowModifSerie.kt
+++ arena/tp1-unqflix/src/main/kotlin/appUnqflix/windows/WindowModifSerie.kt
@@ -1,12 +1,16 @@
 package appUnqflix.windows
 
+import appUnqflix.appModel.SerieAppModel
 import appUnqflix.appModel.UnqflixAppModel
+
 import org.uqbar.arena.kotlin.extensions.*
 import org.uqbar.arena.widgets.*
 import org.uqbar.arena.windows.SimpleWindow
 import org.uqbar.arena.windows.WindowOwner
 import java.awt.Color
 import java.awt.SystemColor.text
+
+
 
 class WindowModifSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWindow<UnqflixAppModel>(owner, model) {
 
@@ -27,10 +31,7 @@ class WindowModifSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWind
                 TextBox(it) with {
                     fontSize = 10
                     width = 200
-//            alignLeft()  // right, left, center
-//
-//            bgColor = Color.orange
-//            bindTo("falta var")
+                 //   bindToModel(thisWindow.modelObject.selectedSerie!!, "title")
                 }
 
             }
@@ -43,10 +44,7 @@ class WindowModifSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWind
                 TextBox(it) with {
                     fontSize = 10
                     width = 200
-//            alignLeft()  // right, left, center
-//
-//            bgColor = Color.orange
-//            bindTo("falta var")
+              //      bindToModel(thisWindow.modelObject.selectedSerie!!, "poster")
                 }
             }
         }
@@ -61,92 +59,88 @@ class WindowModifSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWind
                 KeyWordTextArea(it) with {
                     height = 100
                     width = 150
-//                  bgColor = Color.orange
-//                  bindTo("falta var")
-//                  bindColorTo("blue")
-//                  bindEnabledTo("enabled")
-                  }}
 
-                  Panel(it) with {
-                        asHorizontal()
-                        Label(it) with {
-                        text = "State"
-                        alignLeft()
-                   }
-                      CheckBox(it) with {
-                            //            bindTo("selected")
-//                bindEnabledTo("disabled")
+                  //  bindToModel(thisWindow.modelObject.selectedSerie!!, "descripcion")
+                }
+            }
 
-                        }
+            Panel(it) with {
+                asHorizontal()
+                Label(it) with {
+                    text = "State"
+                    alignLeft()
+                }
+                CheckBox(it) with {
+                    //
 
 
-
-                   }
-        }
+                }
+            }
 
 //////////Panel Categorias//////
 
-        Panel(p0) with {
-            Label(it) with {
-                text = "Categories:"
-                alignLeft()
+            Panel(p0) with {
+                Label(it) with {
+                    text = "Categories:"
+                    alignLeft()
+                }
             }
-        }
-        Panel(p0) with {
-            asHorizontal()
-            KeyWordTextArea(it) with {
-                height = 200
-                width = 70
-                bgColor = Color.orange
+            Panel(p0) with {
+                asHorizontal()
+                KeyWordTextArea(it) with {
+                    height = 200
+                    width = 70
+                    bgColor = Color.orange
 //            bindTo("falta var")
 //            bindColorTo("blue")
 //            bindEnabledTo("enabled")
-            }
-            Panel(it) with {
-                       asVertical()
-                        Button(it) with {
+                }
+                Panel(it) with {
+                    asVertical()
+                    Button(it) with {
                         text = "<"
                         fontSize = 10
-                        }
-                         Button(it) with {
-                            text = ">"
-                            fontSize =10
-                         }
-            }
-            KeyWordTextArea(it) with {
-                width = 70
-                height = 200
+                    }
+                    Button(it) with {
+                        text = ">"
+                        fontSize = 10
+                    }
+                }
+                KeyWordTextArea(it) with {
+                    width = 70
+                    height = 200
 //            bgColor = Color.orange
 //            bindTo("falta var")
 //            bindColorTo("blue")
 //            bindEnabledTo("enabled")
+                }
+
+
             }
 
-
-        }
-
 ///////////////Related Content
-        Panel(p0) with {
-            asHorizontal()
-            Label(it) with {
-                text = "Related content:"
-                alignLeft()
-            }}
-            Panel(p0) with{
-            asHorizontal()
-            KeyWordTextArea(it) with {
-                height = 100
-                width = 180
-                height = 200
-              bgColor = Color.blue
+            Panel(p0) with {
+                asHorizontal()
+                Label(it) with {
+                    text = "Related content:"
+                    alignLeft()
+                }
+            }
+            Panel(p0) with {
+                asHorizontal()
+                KeyWordTextArea(it) with {
+                    height = 100
+                    width = 180
+                    height = 200
+                    bgColor = Color.blue
 //            bindTo("falta var")
 //            bindColorTo("blue")
 //            bindEnabledTo("enabled")
-            }
+                }
                 Panel(it) with {
                     asVertical()
                     Button(it) with {
-                        text ="<"
+                        text = "<"
                         fontSize = 10
                     }
                     Button(it) with {
@@ -165,20 +159,20 @@ class WindowModifSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWind
             }
 
 
-        Panel(p0) with {
-            asHorizontal()
-            Button(it) with {
-                text = "Accept"
-                fontSize = 10
+            Panel(p0) with {
+                asHorizontal()
+                Button(it) with {
+                    text = "Accept"
+                    fontSize = 10
+                }
+
+                Button(it) with {
+                    text = "Cancel"
+                    fontSize = 10
+                }
             }
 
-            Button(it) with {
-                text = "Cancel"
-                fontSize = 10
-            }
+
         }
 
-
-    }
-
-}
+    }}
