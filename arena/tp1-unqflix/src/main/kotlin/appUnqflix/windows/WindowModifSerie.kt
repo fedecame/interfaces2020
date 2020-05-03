53f507916774617bb84e268c39e4d184aba6a8b5
Flavia <flaviaivanafernandez@gmail.com>
2020-05-03 01:04:05 -0300

    Se completo la interfaz de la ventana Modificar Serie

--- arena/tp1-unqflix/src/main/kotlin/appUnqflix/windows/WindowModifSerie.kt
+++ arena/tp1-unqflix/src/main/kotlin/appUnqflix/windows/WindowModifSerie.kt
@@ -6,8 +6,9 @@ import org.uqbar.arena.widgets.*
 import org.uqbar.arena.windows.SimpleWindow
 import org.uqbar.arena.windows.WindowOwner
 import java.awt.Color
+import java.awt.SystemColor.text
 
-class WindowModifSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWindow<UnqflixAppModel>(owner, model){
+class WindowModifSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWindow<UnqflixAppModel>(owner, model) {
 
     override fun addActions(p0: Panel?) {
 
@@ -49,75 +50,120 @@ class WindowModifSerie (owner: WindowOwner, model: UnqflixAppModel) : SimpleWind
                 }
             }
         }
-
-        Panel(p0)with {
+        Label(p0) with {
+            text = "Description:"
+            alignLeft()
+        }
+        Panel(p0) with {
             asHorizontal()
             Panel(it) with {
-                Label(it) with {
-                    text = "Desciption:"
-                    alignLeft()
-                }
 
                 KeyWordTextArea(it) with {
                     height = 100
                     width = 150
-//            bgColor = Color.orange
-//            bindTo("falta var")
-//            bindColorTo("blue")
-//            bindEnabledTo("enabled")
-                }
-            }
+//                  bgColor = Color.orange
+//                  bindTo("falta var")
+//                  bindColorTo("blue")
+//                  bindEnabledTo("enabled")
+                  }}
 
-            Panel(it) with {
-                asHorizontal()
-                Label(it) with {
-                    text = "State"
-                    alignLeft()
-                }
+                  Panel(it) with {
+                        asHorizontal()
+                        Label(it) with {
+                        text = "State"
+                        alignLeft()
+                   }
+                      CheckBox(it) with {
+                            //            bindTo("selected")
+//                bindEnabledTo("disabled")
 
-                CheckBox(it) with {
-                    //            bindTo("selected")
-//            bindEnabledTo("disabled")
+                        }
 
-                }
 
-            }
+
+                   }
         }
 
-
+//////////Panel Categorias//////
 
         Panel(p0) with {
             Label(it) with {
                 text = "Categories:"
                 alignLeft()
             }
-
+        }
+        Panel(p0) with {
+            asHorizontal()
             KeyWordTextArea(it) with {
-                height = 100
-                width = 50
+                height = 200
+                width = 70
+                bgColor = Color.orange
+//            bindTo("falta var")
+//            bindColorTo("blue")
+//            bindEnabledTo("enabled")
+            }
+            Panel(it) with {
+                       asVertical()
+                        Button(it) with {
+                        text = "<"
+                        fontSize = 10
+                        }
+                         Button(it) with {
+                            text = ">"
+                            fontSize =10
+                         }
+            }
+            KeyWordTextArea(it) with {
+                width = 70
+                height = 200
 //            bgColor = Color.orange
 //            bindTo("falta var")
 //            bindColorTo("blue")
 //            bindEnabledTo("enabled")
             }
+
+
         }
 
-
+///////////////Related Content
         Panel(p0) with {
+            asHorizontal()
             Label(it) with {
                 text = "Related content:"
                 alignLeft()
-            }
-
+            }}
+            Panel(p0) with{
+            asHorizontal()
             KeyWordTextArea(it) with {
                 height = 100
-                width = 50
-//            bgColor = Color.orange
+                width = 180
+                height = 200
+              bgColor = Color.blue
 //            bindTo("falta var")
 //            bindColorTo("blue")
 //            bindEnabledTo("enabled")
             }
-        }
+                Panel(it) with {
+                    asVertical()
+                    Button(it) with {
+                        text ="<"
+                        fontSize = 10
+                    }
+                    Button(it) with {
+                        text = ">"
+                        fontSize = 10
+                    }
+                }
+                KeyWordTextArea(it) with {
+                    width = 180
+                    height = 200
+                    bgColor = Color.yellow
+//            bindTo("falta var")
+//            bindColorTo("blue")
+//            bindEnabledTo("enabled")
+                }
+            }
+
 
         Panel(p0) with {
             asHorizontal()
