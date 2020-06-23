c899a334db60a037575557c280e66f5231e109d4
Federico Cameriere <fede.cameriere@gmail.com>
2020-06-23 00:39:05 -0300

    reutilizacion de maqueta pasada a jsx

--- web/unqflix-app/src/RegisterPage.js
+++ web/unqflix-app/src/RegisterPage.js
@@ -1,10 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import './styles/login.css';
+import Footer from './components/Footer';
+import {Link} from 'react-router-dom';
+import pochoclos from './images/popcorn.png'
+import logo from './images/logo.png'
 
 const RegisterPage = (props) => {
     return ( 
-        <div>
-            <h1>RegisterPage</h1>
-        </div>
+        <>
+        <div id="container">
+		    <div id="column">
+                <img id="popcorn" src={pochoclos} alt="Pochoclos"/>
+            </div>
+		    <div id="columnLog">
+			<div id="containerFormRegister">
+				<div id="containerLogo">
+                    <img id="logo"	src={logo} alt="Logo de Unqflix"/><br/>
+                </div>
+				<form action="">
+                    <label>Email:</label><br/>
+                    <input type="email" name="email" required/>
+                    <br/>
+					<label>Name:</label><br/>
+					<input type="text" name="user" required/>
+					<br/>
+					<label>Password: </label><br/>
+					<input type="password" name="pass" required/>
+                    <br/>
+                    <label >Imagelink:</label><br/>
+					<input type="url" name="image"/>
+                    <br/>
+                    <label >Credit Card:</label><br/>
+					<input type="text" name="creditCard"/>
+					<br/><br/>
+					<button id="botonForm">Register</button><br/><br/>
+					<Link to="/login">Back</Link>
+				</form>
+			</div>
+		    </div>
+	    </div>
+        <Footer/>
+        </>
     );
 }
  
