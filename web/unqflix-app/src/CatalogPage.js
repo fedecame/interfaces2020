fa95cd11122357c9d426db48cc74c425101e3460
Federico Cameriere <fede.cameriere@gmail.com>
2020-06-28 17:23:18 -0300

    carrusel de banners y pruebas

--- web/unqflix-app/src/CatalogPage.js
+++ web/unqflix-app/src/CatalogPage.js
@@ -3,15 +3,21 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import CarouselBanners from './components/CarouselBanners';
+import { useHistory } from 'react-router-dom';
 
 const CatalogPage = (props) => {
+    const history = useHistory();
     return (
         <Container>
             <Row>
                 <Col>
                 <h1>CatalogPage</h1>
-                <Button variant="dark">Dark</Button>
+                <CarouselBanners/>
                 </Col>
+            </Row>
+            <Row>
+                <Button variant="dark" onClick={() => history.push("/login")}>Login</Button>
             </Row>
         </Container>
     );
