a98f258fe4c593c4d545aa5bf5e45e37014bdc77
Federico Cameriere <fede.cameriere@gmail.com>
2020-07-02 23:37:05 -0300

    lista pagina resultado de busquedas

--- web/unqflix-app/src/SearchResultsPage.js
+++ web/unqflix-app/src/SearchResultsPage.js
@@ -5,14 +5,20 @@ import GridGenerator from './components/GridGenerator';
 import GridCard from './components/GridCard';
 import apiConsumer from "./ApiConsumer";
 import { useLocation } from 'react-router-dom';
+import './styles/catalog.scss';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const SearchResultsPage = () => {
     const location = useLocation();
     const [searchResult, setSearchResult] = useState([]);
+    const [searchedText, setSearchedText] = useState("");
 
     useEffect(() => {
         const queryParams = new URLSearchParams(location.search);
-        fetchSearchResult(queryParams.get("text"));
+        setSearchedText(queryParams.get("text"));
+        fetchSearchResult(searchedText);
     }, [location]);
 
     const fetchSearchResult = searchText => {
@@ -30,6 +36,13 @@ const SearchResultsPage = () => {
     return (
         <>
             <Header />
+            <Container fluid className="bg-dark">
+                <Row>
+                    <Col>
+                    {searchResult.length > 0 && <div><h3 className="px-2 font-weight-bold text-light margin-for-fixed-header bg-dark">{`Searched text: ${searchedText}`}</h3></div>}
+                    </Col>
+                </Row>
+            </Container>
             {
             searchResult.length > 0 &&
                 <GridGenerator colAmount={6}>
